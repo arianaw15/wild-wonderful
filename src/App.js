b9f3bf098a77107f2aca30c8362718fca31a6566
Ariana Winters <arianaw15@gmail.com>
2021-06-17 20:56:12 -0600

    React bootstrap and navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-import Landing from './components/landingPage/Landing'
+import Landing from './components/landingPage/Landing';
+import NavBar from './components/navBar/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Landing/>
     </div>
   );
