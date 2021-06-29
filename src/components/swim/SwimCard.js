bb7e673264b6e00ef0286c7427c527c0362b3cb7
Ariana Winters <arianaw15@gmail.com>
2021-06-28 19:11:49 -0600

    updated swim cards with img placeholders

--- src/components/swim/SwimCard.js
+++ src/components/swim/SwimCard.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import {Card,Button} from 'react-bootstrap';
+import './swim.css';
+import img from './swimsuit.jpg';
+
 
 function SwimCard() {
     return (
         <div>
-            <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
+<Card style={{ width: 'auto' }}>
+  <Card.Img class="img" variant="top" src={img} />
   <Card.Body>
-    <Card.Title>Card Title</Card.Title>
+    <Card.Title>The Jane</Card.Title>
     <Card.Text>
       Some quick example text to build on the card title and make up the bulk of
       the card's content.
     </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
+    <Button variant="primary">Add to Cart</Button>
   </Card.Body>
 </Card>
         </div>
